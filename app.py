81acdcfb0519e3a75c3875222c87f4e93e1f8171
Oussama Sassour <83518153+Ouss77@users.noreply.github.com>
2022-09-26 17:55:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,34 +1,53 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_mysqldb import MySQL
 import mysql.connector
+from prettytable import from_db_cursor
 from mysql.connector.errors import Error
 
 app = Flask(__name__)
 
+conDatabase = {
+            'host': '127.0.0.1',
+            'database': 'employees',
+            'user': 'root',
+            'password': 'intellia@2022'
+        }
+try:
+  db = mysql.connector.connect(**conDatabase)
+  print("the database is connected ")
+except Error as error:
+  print("Error!")
+  print(error)
 
-app.config['MYSQL_HOST'] = '127.0.0.1'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'intellia@2022'
-app.config['MYSQL_DB'] = 'employee'
-
-mysql = MySQL(app)
-
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def login():
     error = None
-    if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            error = 'Invalid Credentials. Please try again.'
-        else:
-            return redirect(url_for('home'))
-    return render_template('login.html', error=error)
 
-@app.route('/users')
-def users():
-    cur = mysql.connection.cursor()
-    resultValue = cur.execute("SELECT * FROM employee_data")
-    userDetails = cur.fetchall()
-    return render_template('users.html', userDetails=userDetails)
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or \
+           request.form['password'] != 'admin':
+            error = 'Invalid username or password. Please try again !'
+        else:
+            # flashes on successful login
+            flash('You were successfully logged in')
+            return redirect(url_for('condition'))
+    return render_template('login.html', error=error)
+@app.route('/hist')
+def initial():
+    return render_template('hist.html')
+@app.route('/hist', methods=['POST'])
+def condition():
+    if request.method == 'POST':
+        variable = request.form['variable']
+        #date_task = request.form['date_task']
+        cur = db.cursor()
+        query1 = "SELECT * FROM intellia where lots = (%s)"
+        #query2 = "SELECT * FROM intellia where date = (%s)"
+        cur.execute(query1, (variable,))
+        #cur.execute(query2, (date_task,))
+        userDetails = cur.fetchall()
+        return render_template('hist.html', userDetails=userDetails)
 
 if __name__ == '__main__':
+    app.secret_key = 'super secret key'
     app.run(debug=True)
